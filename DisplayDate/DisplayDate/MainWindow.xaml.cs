7bbe6a73d91b7431778112da017ff2e954cc844f
RacheliGo <racheli5910@gmail.com>
2024-01-31 23:20:39 +0200

    Change date and time format

--- DisplayDate/DisplayDate/MainWindow.xaml.cs
+++ DisplayDate/DisplayDate/MainWindow.xaml.cs
@@ -24,7 +24,7 @@ namespace DisplayDate
         public MainWindow()
         {
             InitializeComponent();
-            DateLable.Content = newTime;
+            DateLable.Content = newTime.ToString();
         }
     }
 }
